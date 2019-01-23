d26da0c266bfdca31d77de56f47e98ed83a0f5b9
Matt Bernhardt <mjbernha@mit.edu>
2019-01-23 13:47:31 -0500

    Adds date range, description, and sitewide fields

--- schemas/alert.js
+++ schemas/alert.js
@@ -9,6 +9,26 @@ export default {
 			type: 'string'
 		},
 		{
+			name: 'description',
+			title: 'Description',
+			type: 'string'
+		},
+		{
+			name: 'startAt',
+			title: 'Alert Starts At',
+			type: 'datetime'
+		},
+		{
+			name: 'endsAt',
+			title: 'Alert Ends At',
+			type: 'datetime'
+		},
+		{
+			name: 'sitewide',
+			title: 'Show Alert Sitewide',
+			type: 'boolean'
+		},
+		{
 			name: 'publishedAt',
 			title: 'Published at',
 			type: 'datetime'
